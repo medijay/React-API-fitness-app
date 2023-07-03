7e494a699c2e94a0946c0e4fcb27ab60820625af
Ahmed Adekunle <medijay99@yahoo.com>
2023-07-03 15:22:09 +0100

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => {
+  return (
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='200px' height='40px' />
+        <Typography
+          variant='h5'
+          sx={{ fontSize: { lg: '28px', xs: '20px' } }}
+          textAlign='center'
+          pb='40px'
+          mt='20px'
+        >
+          Made with ❤️ by Medijay
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
